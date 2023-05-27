95377a153c78870c42eb33902de12ad47732d3ef
Alexander Frisch <ali@crunchtime.dev>
2023-05-27 20:22:38 +0200

    use alias for fonts (might have to be adapted for dev compilation

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import houdini from 'houdini/vite';
 import type { UserConfig } from 'vite';
+import { resolve } from 'path';
 
 const config: UserConfig = {
   plugins: [houdini(), sveltekit()],
+  resolve: {
+    alias: {
+      $fonts: resolve('./static/fonts')
+    }
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
 };
 export default config;
+
