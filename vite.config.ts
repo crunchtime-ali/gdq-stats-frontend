05491128181745f14d18d31b5b3a2b11ee75da42
René Fermann <rene.fermann@gmx.de>
2023-06-06 22:08:23 +0200

    fix(fonts): fix font serving path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import houdini from 'houdini/vite';
-import type { UserConfig } from 'vite';
 import { resolve } from 'path';
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
   plugins: [houdini(), sveltekit()],
   resolve: {
     alias: {
-      $fonts: resolve('./static/fonts')
+      $fonts: resolve('./fonts')
     }
   },
   test: {
@@ -15,4 +15,3 @@ const config: UserConfig = {
   }
 };
 export default config;
-
