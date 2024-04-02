b24dab3c8906d2b16f826a3b2ffaa0b3e698684d
rfermann <18217019+rfermann@users.noreply.github.com>
2024-04-02 20:31:07 +0700

    fix(chart navigation): only set cookies on actual naviation and not during data preloading

--- src/components/Event/LiveStats/ChartLink.svelte
+++ src/components/Event/LiveStats/ChartLink.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-  import { preloadData } from '$app/navigation';
+  import { goto, preloadData } from '$app/navigation';
   import type { EventDataType$options } from '$houdini';
   import { onMount } from 'svelte';
-  import { CHART_PARAMETER } from '../../../lib/constants';
+  import Cookies from 'js-cookie';
+
+  import { CHART_PARAMETER } from '$lib/constants';
 
   export let activeChart: EventDataType$options;
   export let chart: EventDataType$options;
@@ -10,22 +12,29 @@
 
   const href = `?${CHART_PARAMETER}=${chart.toLowerCase()}`;
 
+  const navigate = () => {
+    Cookies.set(CHART_PARAMETER, chart.toLowerCase(), { path: '/' });
+    goto(href, {
+      noScroll: true
+    });
+  };
+
   onMount(() => {
     preloadData(href);
   });
 </script>
 
-<a
-  data-sveltekit-preload-data
-  {href}
+<li
+  class={`mt-0.5 px-4 py-2.5 ${
+    activeChart.toLowerCase() === chart.toLowerCase()
+      ? 'bg-primary text-darkGray'
+      : 'text-mediumDarkGray hover:bg-lightGray'
+  }`}
 >
-  <li
-    class={`mt-0.5 px-4 py-2.5 ${
-      activeChart.toLowerCase() === chart.toLowerCase()
-        ? 'bg-primary text-darkGray'
-        : 'text-mediumDarkGray hover:bg-lightGray'
-    }`}
+  <button
+    on:click={navigate}
+    class="w-full text-left"
   >
     {name}
-  </li></a
->
+  </button>
+</li>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
-import { EventDataType, type EventDataType$options } from '../../$houdini';
-import { CHART_PARAMETER } from '../lib/constants';
+import { EventDataType, type EventDataType$options } from '$houdini';
+import { CHART_PARAMETER } from '$lib/constants';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ cookies, url }) => {
@@ -10,24 +10,31 @@ export const load: PageServerLoad = async ({ cookies, url }) => {
   const eventDataTypes = Object.values(EventDataType);
 
   if (searchParamVal) {
+    // unsupported chart: redirect to first chart
     if (!eventDataTypes.includes(searchParamVal.toUpperCase() as EventDataType$options)) {
-      cookies.set(CHART_PARAMETER, viewersVal);
+      cookies.set(CHART_PARAMETER, viewersVal, { path: '/', httpOnly: false });
       throw redirect(302, `${url.pathname}?${CHART_PARAMETER}=${viewersVal}`);
     }
 
-    cookies.set(CHART_PARAMETER, searchParamVal);
-  }
-
-  if (!searchParamVal) {
+    // supported chart: set cookie before redirecting, if no other cookie exists yet
+    // (note: load function is also called while preloading data with no way to tell
+    // if the current request is a preload or an actual navigation)
+    if (!cookieVal) {
+      cookies.set(CHART_PARAMETER, searchParamVal, { path: '/', httpOnly: false });
+    }
+  } else {
     if (cookieVal) {
+      // supported chart in cookie: redirect to chart
       if (eventDataTypes.includes(cookieVal.toUpperCase() as EventDataType$options)) {
         throw redirect(302, `${url.pathname}?${CHART_PARAMETER}=${cookieVal}`);
       }
 
-      cookies.set(CHART_PARAMETER, viewersVal);
+      // unsupported chart in cookie: redirect to first chart
+      cookies.set(CHART_PARAMETER, viewersVal, { path: '/', httpOnly: false });
       throw redirect(302, `${url.pathname}?${CHART_PARAMETER}=${viewersVal}`);
     } else {
-      cookies.set(CHART_PARAMETER, viewersVal);
+      // no chart in cookie or no cookie: redirect to first chart
+      cookies.set(CHART_PARAMETER, viewersVal, { path: '/', httpOnly: false });
       throw redirect(302, `${url.pathname}?${CHART_PARAMETER}=${viewersVal}`);
     }
   }
