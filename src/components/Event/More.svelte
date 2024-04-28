2064149bec104a1ed3957f3d66db76932f008101
rfermann <18217019+rfermann@users.noreply.github.com>
2024-04-28 22:58:18 +0200

    fix: include active chart type into links for alternative events

--- src/components/Event/More.svelte
+++ src/components/Event/More.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-  import type { getEventInformation$result } from '$houdini';
+  import { EventDataType, type EventDataType$options, type getEventInformation$result } from '$houdini';
   import A from '../Layout/A.svelte';
   import Section from '../Layout/Section.svelte';
   import Container from '../Shared/Container.svelte';
   import H3 from '../Shared/H3.svelte';
+  import { page } from '$app/stores';
+  import { CHART_PARAMETER } from '$lib/constants';
 
   export let event: getEventInformation$result['getEvent'];
   export let alternativeEvents: getEventInformation$result['getAlternativeEvents'];
 
+  $: activeChart =
+    ($page.url.searchParams.get(CHART_PARAMETER) as EventDataType$options) ?? EventDataType.VIEWERS;
+
   const buildLink = (year: number, name: string) =>
-    `/previous-events/${year}/${name.toLocaleLowerCase()}`;
+    `/previous-events/${year}/${name.toLocaleLowerCase()}?${CHART_PARAMETER}=${activeChart}`;
 </script>
 
 <Section title="more from gdqstats">
@@ -41,15 +46,17 @@
       <div class="w-1/2 px-4">
         <H3 text="more events" />
         <ul class="mb-2.5 list-disc pl-10">
-          {#each alternativeEvents as alternativeEvent}
-            <li>
-              <A
-                href={`${buildLink(alternativeEvent.year, alternativeEvent.eventType.name)}`}
-                className="text-[16px] "
-              >{`${alternativeEvent.eventType.name} ${alternativeEvent.year}`}</A
-              >
-            </li>
-          {/each}
+          {#key activeChart}
+            {#each alternativeEvents as alternativeEvent}
+              <li>
+                <A
+                  href={`${buildLink(alternativeEvent.year, alternativeEvent.eventType.name)}`}
+                  className="text-[16px] "
+                >{`${alternativeEvent.eventType.name} ${alternativeEvent.year}`}</A
+                >
+              </li>
+            {/each}
+          {/key}
         </ul>
       </div>
     </Container>
